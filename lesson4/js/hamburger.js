b253fe82548a43c62283cc2dfe86a20e294a0cec
jpgoldenii <gol79001@byui.edu>
2020-05-16 21:25:07 -0400

    update

--- lesson4/js/hamburger.js
+++ lesson4/js/hamburger.js
@@ -1,6 +1,4 @@
-const hambutton = document.querySelector(".burger");
-hambutton.addEventListener("click", toggleMenu);
-
+  
 function toggleMenu() {
-  document.querySelector(".navigation").classList.toggle("responsive");
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
