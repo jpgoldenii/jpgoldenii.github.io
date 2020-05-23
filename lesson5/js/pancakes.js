907dce9109a201acb9e25f5bbb158740447ed040
jpgoldenii <gol79001@byui.edu>
2020-05-23 16:29:44 -0400

    lesson5

---
+++ lesson5/js/pancakes.js
@@ -0,0 +1,7 @@
+var d = new Date();
+const banner = document.getElementById("pancakes");
+if (d.getDay() == 5) {
+    banner.style.display = "block";
+} else {
+    banner.style.display = "none";
+}
