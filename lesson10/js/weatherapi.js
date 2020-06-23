d65a3a5fe0b9ad2624be8734baa8e7e863853239
jpgoldenii <gol79001@byui.edu>
2020-06-22 20:16:17 -0400

    openweathermap

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,14 +3,10 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-
         document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
     });
-
-  
-
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-const desc = jsObject.weather[0].description;
-document.getElementById('imagesrc').textContent = imagesrc;
-document.getElementById('icon').setAttribute('src', imagesrc);
-document.getElementById('icon').setAttribute('alt', desc);
