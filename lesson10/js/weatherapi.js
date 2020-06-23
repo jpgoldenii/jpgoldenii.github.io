8d34f852cdfc33bedea7ccaddcbb501b127713a9
jpgoldenii <gol79001@byui.edu>
2020-06-22 20:41:31 -0400

    openweathermap

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=07d41ea5aa3c8676e88469ed8ffafeeb";
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=07d41ea5aa3c8676e88469ed8ffafeeb';
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
