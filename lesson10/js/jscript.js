84c59da06bbfd606ba9bc9a9eea579796328646d
jpgoldenii <gol79001@byui.edu>
2020-06-25 15:20:34 -0400

    Preston-10  5-day

--- lesson10/js/jscript.js
+++ lesson10/js/jscript.js
@@ -152,13 +152,20 @@ const apiURL2 = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&uni
 fetch(apiURL2)
     .then((response) => response.json())
     .then((jsObject) => {
-        const forecasts = jsObject.list.filter(x =>
-            x.dt_txt.includes(`18:00:00`));
-        for (let i = 0; i < forecasts.length; i++) {
-            document.getElementById(`day${i+1}High`).textContent = Math.round(forecasts[i].main.temp, 0);
-            const imagesrc = 'https://openweathermap.org/img/wn/' + forecasts[i].weather[0].icon + '.png';
-            const desc = forecasts[i].weather[0].description;
-            document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
-            document.getElementById(`icon${i+1}`).setAttribute('alt', desc);
-        }
+        //console.log(jsObject);
+        const forecasts = jsObject.list.filter(x => x.dt_txt.includes(`18:00:00`));
+        console.log(forecasts);
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let day = 0;
+        forecasts.forEach(forecast => {
+            let wd = new Date(forecast.dt_txt);
+            document.getElementById(`hightemp${day+1}`).textContent = Math.round(forecast.main.temp, 0);
+            document.getElementById(`day${day+1}`).textContent = weekdays[wd.getDay()];
+            const imagesrc = 'https://openweathermap.org/img/wn/' + forecast.weather[0].icon + '.png';
+            const desc = forecast.weather[0].description;
+            document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+            document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+            day++;
+
+        });
     });
